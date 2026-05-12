4e54111b7452f49f8da74f3c88003f17ff338bfc
Sergey Mikhailov <serg.m1hailov@yandex.ru>
2026-05-12 12:03:43 +0500

    Initial Commit

---
+++ ReShare/ReShareApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReShareApp.swift
+//  ReShare
+//
+//  Created by Михайлов Сергей Антонович on 12.05.2026.
+//
+
+import SwiftUI
+
+@main
+struct ReShareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
